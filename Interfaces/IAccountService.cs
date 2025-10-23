a2e3bb961377a2e76b292f8399a0afe2bc5a10be
RudyChemik <klefedronowelove@gmail.com>
2025-10-24 00:14:26 +0200

    feat: [GET] /Account/get-company-profile

--- Interfaces/IAccountService.cs
+++ Interfaces/IAccountService.cs
@@ -1,10 +1,12 @@
 ﻿using fuszerkomat_api.Data;
 using fuszerkomat_api.VM;
+using fuszerkomat_api.VMO;
 
 namespace fuszerkomat_api.Interfaces
 {
     public interface IAccountService
     {
+        Task<Result<CompanyProfileVMO>> GetCompanyProfileAsync(string id, CancellationToken ct);
         Task<Result> UpdateCompanyInfrormation(string userId, CompanyProfileInfoVM model, CancellationToken ct);
         Task<Result> UpdateUserInformation(string userId, UserProfileInfoVM model, CancellationToken ct);
         Task<Result> DeleteAccount(string userId, CancellationToken ct);
