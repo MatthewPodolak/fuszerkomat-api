17a9597a8b207152b7cb64ca9f7e8012b744658e
RudyChemik <klefedronowelove@gmail.com>
2025-10-24 10:19:59 +0200

    feat: mongo init

---
+++ Data/Models/Chat/Conversation.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+
+namespace fuszerkomat_api.Data.Models.Chat
+{
+    public class Conversation
+    {
+        public ObjectId Id { get; set; }
+        public int TaskId { get; set; }
+        public string OwnerUserId { get; set; } = default!;
+        public string CompanyUserId { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastMessageAt { get; set; }
+    }
+}
---
+++ Data/Models/Chat/Message.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson;
+
+namespace fuszerkomat_api.Data.Models.Chat
+{
+    public class Message
+    {
+        public ObjectId Id { get; set; }
+        public ObjectId ConversationId { get; set; }
+        public string SenderId { get; set; } = default!;
+        public string Text { get; set; } = default!;
+        public DateTime CreatedAt { get; set; }
+    }
+}
