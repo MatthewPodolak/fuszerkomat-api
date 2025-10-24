1b84331accac4820349a85ec23d658fec4663f01
RudyChemik <klefedronowelove@gmail.com>
2025-10-24 21:24:18 +0200

    feat: [GET] /UserWorkTasks/change-application-status

---
+++ VM/ChangeApplicationStatusVM.cs
@@ -0,0 +1,22 @@
+﻿using fuszerkomat_api.Data.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace fuszerkomat_api.VM
+{
+    public class ChangeApplicationStatusVM
+    {
+        public int WorkTaskId { get; set; }
+
+        [Required]
+        public string CompanyId { get; set; }
+
+        [Required]
+        public AnswerAplication Answer { get; set; }
+    }
+
+    public enum AnswerAplication
+    {
+        Accept,
+        Reject
+    }
+}
